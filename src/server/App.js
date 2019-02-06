63874fcb687f2dea2d9f95a84d89482d25888758
Eugene Koval <iam.eugenekoval@gmail.com>
2019-02-06 16:54:21 +0200

    upd server app

--- src/server/App.js
+++ src/server/App.js
@@ -2,14 +2,37 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const http = require("http");
 const socketIo = require("socket.io");
+const fs = require("fs");
+const path = require("path");
+
+const PATH_PUBLIC = `${__dirname}/../../public`;
+const PATH_DB = `${__dirname}/../database`;
+
+const database = path.join(`${PATH_DB}/index.json`);
 
 class App {
     constructor() {
         this.express = express();
         this.express.use(bodyParser.json());
         this.server = http.createServer(this.express);
-        this.ws = socketIo(this.server);
+        this.io = socketIo(this.server);
+
+        this.mountRoutes();
+    }
+
+    mountRoutes() {
+        this.express.get("/db", (req, res) => {
+            fs.readFile(database, "utf8", (err, data) => {
+                res.end(data);
+            });
+        });
+
+        this.express.get("/", (req, res) => {
+            fs.readFile(path.join(`${PATH_PUBLIC}/index.html`), "utf8", (err, data) => {
+                res.end(data);
+            });
+        });
     }
 }
 
-module.export = new App();
+module.exports = new App();
--- src/server/index.js
+++ src/server/index.js
@@ -1,31 +1,10 @@
-const express = require("express");
-const fs = require("fs");
-const path = require("path");
-const bodyParser = require("body-parser");
-
-const http = require("http");
-const socketIo = require("socket.io");
-
 const chalk = require("chalk");
 const axios = require("axios");
 
 const PORT = 8081;
-const PATH_PUBLIC = `${__dirname}/../../public`;
-const PATH_DB = `${__dirname}/../database`;
 const PATH_API = "https://jsonplaceholder.typicode.com/todos/2";
 
-const database = path.join(`${PATH_DB}/index.json`);
-
-class App {
-    constructor() {
-        this.express = express();
-        this.express.use(bodyParser.json());
-        this.server = http.createServer(this.express);
-        this.io = socketIo(this.server);
-    }
-}
-
-const app = new App();
+const app = require("./App");
 
 app.io.on("connection", socket => {
     console.log("New client connected");
@@ -42,16 +21,6 @@ const getApiAndEmit = async socket => {
     }
 };
 
-app.server.listen(PORT, () => console.log(chalk.black.bgCyan(`Listening on http://127.0.0.1:${PORT}/`)));
-
-app.express.get("/db", (req, res) => {
-    fs.readFile(database, "utf8", (err, data) => {
-        res.end(data);
-    });
-});
-
-app.express.get("/", (req, res) => {
-    fs.readFile( path.join(`${PATH_PUBLIC}/index.html`), "utf8", (err, data) => {
-        res.end(data);
-    });
+app.server.listen(PORT, () => {
+    console.log(chalk.black.bgCyan(`Listening on http://127.0.0.1:${PORT}/`))
 });
